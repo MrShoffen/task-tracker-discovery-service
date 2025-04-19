22591d0f331d30b2e143dc4cb1fb6400daf56ed4
andrey_vasiliev <mrshoffen@gmail.com>
2025-04-20 02:00:17 +0300

    initial commit

---
+++ src/main/java/org/mrshoffen/tasktracker/discoveryservice/TaskTrackerDiscoveryServiceApplication.java
@@ -0,0 +1,16 @@
+package org.mrshoffen.tasktracker.discoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class TaskTrackerDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(TaskTrackerDiscoveryServiceApplication.class, args);
+    }
+
+}
